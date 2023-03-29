e14eb73c4b461e39bb9466989ad5570840e59331
Lucas Assmann <lucasassmann@outlook.com>
2023-03-29 00:11:05 -0300

    adjusted register_view to redirect to root after success.

--- pontoCalculator/funcionario/views.py
+++ pontoCalculator/funcionario/views.py
@@ -16,15 +16,7 @@ def register_view(request):
         form = CustomUserCreationForm(request.POST)
         if form.is_valid():
             form.save()
-            username = form.cleaned_data.get('username')
-            password = form.cleaned_data.get('password1')
-            user = authenticate(request, username=username, password=password)
-            if user is not None:
-                login(request, user)
-                messages.success(request, f'Account created for {username}!')
-                return redirect('/')
-            else:
-                messages.error(request, 'Invalid username or password')
+            return redirect('/')
     else:
         form = CustomUserCreationForm()
     return render(request, 'register.html', {'form': form})
