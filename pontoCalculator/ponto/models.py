fe675b56da4ebf2941120356556ba1a3b999db1d
lucassmann <lucasassmann@outlook.com>
2023-04-24 15:54:52 -0300

    tipo e detalhes includios no model RegistroPonto

--- pontoCalculator/ponto/models.py
+++ pontoCalculator/ponto/models.py
@@ -5,3 +5,5 @@ from funcionario.models import CustomUser
 class RegistroPonto(models.Model):
     funcionario = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
     data_hora = models.DateTimeField(auto_now_add=True)
+    tipo = models.CharField(max_length=10, choices=(('entrada', 'Entrada'), ('saída', 'Saída')))
+    detalhes = models.CharField(max_length=100, blank=True, null=True)
