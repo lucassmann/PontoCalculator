582ee35b39a65f244d9c310d49f80fb87729faa0
lucassmann <lucasassmann@outlook.com>
2023-04-24 12:16:34 -0300

    dev env username fix

--- pontoCalculator/pontoCalculator/settings.py
+++ pontoCalculator/pontoCalculator/settings.py
@@ -82,7 +82,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'my_database',
-        'USER': 'root',
+        'USER': 'user',
         'PASSWORD': 'pass',
         'HOST': '127.0.0.1',
         'PORT': '3306',
