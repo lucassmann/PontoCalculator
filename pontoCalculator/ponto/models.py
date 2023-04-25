ecf740e23823fb320e205114e6ee2b59689c788f
lucassmann <lucasassmann@outlook.com>
2023-04-25 12:46:23 -0300

    mudança de field auto_now para default a fim de inserir datas arbitrarias.

--- pontoCalculator/ponto/models.py
+++ pontoCalculator/ponto/models.py
@@ -1,9 +1,10 @@
 
 from django.db import models
 from funcionario.models import CustomUser
+from django.utils import timezone
 
 class RegistroPonto(models.Model):
     funcionario = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
-    data_hora = models.DateTimeField(auto_now_add=True)
-    tipo = models.CharField(max_length=10, choices=(('entrada', 'Entrada'), ('saída', 'Saída')))
+    data_hora = models.DateTimeField(default=timezone.now, null=False)
+    tipo = models.CharField(max_length=10, choices=(('entrada', 'Entrada'), ('saída', 'Saída'), ('excessao', 'excessão' 'Excessão')))
     detalhes = models.CharField(max_length=100, blank=True, null=True)
